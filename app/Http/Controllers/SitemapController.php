5c12cd8f22774a2c900f70820f92bb0944fef208
Motiur Rahman <112879061+dev-cmt@users.noreply.github.com>
2026-03-18 11:53:40 +0600

    int update

---
+++ app/Http/Controllers/SitemapController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Page;
+use App\Models\Product;
+use App\Models\Blog;
+use App\Models\Service;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Cache;
+
+class SitemapController extends Controller
+{
+    /**
+     * Serve sitemap.xml
+     */
+    public function index()
+    {
+        $data = [
+            'pages'     => Page::select('id', 'slug', 'updated_at')->get(),
+            // 'blogs'     => Blog::select('id', 'slug', 'updated_at')->where('status', 1)->get(),
+            'lastmod'   => Carbon::now()->toIso8601String(), // Sitemap last update
+        ];
+
+        return response()->view('sitemap.index', $data)->header('Content-Type', 'application/xml; charset=UTF-8');
+    }
+}
